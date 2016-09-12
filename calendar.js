2fd4d15579448744b25e4fb9f7b8db0f5f5852d2
Alex Griffin <Alex Griffin>
2016-09-11 21:34:09 -0500

    Added a method which automatically generates the month view (sets the Month text, puts the correct numbers in the correct days of the week.

--- calendar.js
+++ calendar.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() { //initiated when the page is first loaded
 	
+	var date = new Date();
+	var day_of_month = date.getDate();
+	
+	generateMonthView(date.getMonth(), date.getFullYear());
+	
 	 $('.days').click(function(e){
         e.preventDefault();
 		
@@ -8,5 +13,47 @@ $( document ).ready(function() { //initiated when the page is first loaded
 			
 			
       });
+	  
+	  $('#button_year').click(function(e){
+        e.preventDefault();
+		
+		window.location.href = "YearView.html";
+			
+			
+      });
+	  
 	
 });
+
+function generateMonthView(month, year){
+	
+	var dummy_date = new Date(year, month, 1, 0, 0, 0, 0);
+	var first_day_of_week = dummy_date.getDay();
+	
+	$('.monthHeader').text(getMonthFromNum(dummy_date.getMonth()));
+	
+	var number = 1;
+	for(i=first_day_of_week;i< (first_day_of_week + getDaysFromMonthNum(dummy_date.getMonth())); i++){
+		
+		var table_id = "#day_".concat(i);
+		
+		$(table_id).text(number);
+		number++;
+	} 
+
+}
+
+function getDayFromNum(num){
+	
+	return ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'][num];
+}
+
+function getMonthFromNum(num){
+	
+	return ['January','February','March','April','May','June','July','August','September','October','November','December'][num];
+}
+
+function getDaysFromMonthNum(num){
+	
+	return [31,28,31,30,31,30,31,31,30,31,30,31][num];
+}
