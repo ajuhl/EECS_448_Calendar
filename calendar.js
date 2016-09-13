9b0282459a1ab616903027f4daa4c5994ccf3a31
Alex Griffin <Alex Griffin>
2016-09-12 21:27:00 -0500

    Adjusted the page somewhat to account for the new day modal. It pops up now with no information. I moved the next and previous month buttons to the side, and hooked them up so they function correctly.

--- calendar.js
+++ calendar.js
@@ -2,25 +2,58 @@ $( document ).ready(function() { //initiated when the page is first loaded
 	
 	var date = new Date();
 	var day_of_month = date.getDate();
+	var modal = document.getElementById('dayModal');
 	
 	generateMonthView(date.getMonth(), date.getFullYear());
 	
-	 $('.days').click(function(e){
-        e.preventDefault();
+	$('.days').click(function(e){
 		
+		e.preventDefault();
+
 		$('.days').css('color','white');
 		$(this).css('color', 'black');
-			
-			
-      });
+
+		modal.style.display = "block";
+		
+		
+	});
 	  
-	  $('#button_year').click(function(e){
-        e.preventDefault();
+	$('.close').click(function(e){
+		
+		e.preventDefault();
+
+		modal.style.display = "none";
+			
+	});
+	  
+	$('#button_year').click(function(e){
+        
+		e.preventDefault();
 		
 		window.location.href = "YearView.html";
 			
 			
-      });
+    });
+	
+	$('.buttonNextMonth').click(function(e){
+		
+		e.preventDefault();
+
+		date.setMonth(date.getMonth()+1);
+		generateMonthView(date.getMonth(), date.getFullYear());
+		$('.days').css('color','white');
+			
+	});
+	
+	$('.buttonPreviousMonth').click(function(e){
+		
+		e.preventDefault();
+
+		date.setMonth(date.getMonth()-1);
+		generateMonthView(date.getMonth(), date.getFullYear());
+		$('.days').css('color','white');
+			
+	});
 	  
 	
 });
@@ -30,7 +63,15 @@ function generateMonthView(month, year){
 	var dummy_date = new Date(year, month, 1, 0, 0, 0, 0);
 	var first_day_of_week = dummy_date.getDay();
 	
-	$('.monthHeader').text(getMonthFromNum(dummy_date.getMonth()));
+	$('.monthHeader').text(getMonthFromNum(dummy_date.getMonth()) + " " + dummy_date.getFullYear());
+	
+	for(i=0;i<=41;i++){
+		
+		var table_id = "#day_".concat(i);
+		
+		$(table_id).text("");
+		number++;
+	}
 	
 	var number = 1;
 	for(i=first_day_of_week;i< (first_day_of_week + getDaysFromMonthNum(dummy_date.getMonth())); i++){
