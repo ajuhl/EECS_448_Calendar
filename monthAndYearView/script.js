df612823a2e270235cd836de5956190d8ad83f69
Alex Griffin <Alex Griffin>
2016-09-14 12:24:33 -0500

    Cleaning out all of the extra stuff.

--- monthAndYearView/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-	var d =  new Date();
-	var month = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
-	var month2 = d.getMonth();
-	
-
-}
