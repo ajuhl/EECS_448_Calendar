794a62753e0d8f0f65aef1db329b0164a18eef37
Scott Diffin <diffin@ku.edu>
2016-10-05 16:59:24 -0500

    Added repeat functionality to getEvents.php

--- calendar.sql
+++ calendar.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 03, 2016 at 07:05 PM
+-- Generation Time: Oct 05, 2016 at 09:47 PM
 -- Server version: 5.1.73
 -- PHP Version: 5.3.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ajuhl`
+-- Database: `sdiffin`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `calendar` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `start` datetime NOT NULL,
   `stop` datetime NOT NULL,
-  `repeat` varchar(8) NOT NULL,
+  `repeat` varchar(8) NOT NULL DEFAULT 'n',
   `event` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `month` (`start`,`stop`)
@@ -41,30 +41,30 @@ CREATE TABLE IF NOT EXISTS `calendar` (
 --
 
 INSERT INTO `calendar` (`id`, `start`, `stop`, `repeat`, `event`) VALUES
-(59, '2016-10-03 01:00:00', '2016-10-03 02:00:00', 'm', 'Alaska Day'),
-(57, '2016-09-21 12:30:00', '2016-09-21 23:30:00', 'b123', 'September equinox'),
-(58, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Columbus Day'),
-(54, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Commencement'),
-(55, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Labor Day Holiday - No classes'),
-(52, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'First day of Finals'),
-(53, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Last day of Finals'),
-(50, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Last day of classes'),
-(51, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Stop Day'),
-(49, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Last Day of Spring Break'),
-(47, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Last day to enroll online'),
-(48, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'First day of Spring Break'),
-(46, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'First day of classes'),
-(42, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Thanksgiving Break Last Day'),
-(45, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Enrollment Appointments begin'),
-(41, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Thanksgiving Break First Day'),
-(39, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Stop Day'),
-(40, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Last day of classes'),
-(38, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'First day of fall break'),
-(37, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Last Day of Fall Break'),
-(36, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'First Day of Fall Break'),
-(35, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'All of our code is due!'),
-(44, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Diplomas available'),
-(43, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', 'Nicks Birthday');
+(59, '2016-10-03 01:00:00', '2016-10-03 02:00:00', 'b135', 'Alaska Day'),
+(57, '2016-09-21 12:30:00', '2016-09-21 23:30:00', 'n', 'September equinox'),
+(58, '2017-03-10 00:00:00', '2017-03-13 00:00:00', 'n', 'Columbus Day'),
+(54, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Commencement'),
+(55, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Labor Day Holiday - No classes'),
+(52, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'First day of Finals'),
+(53, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Last day of Finals'),
+(50, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Last day of classes'),
+(51, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Stop Day'),
+(49, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Last Day of Spring Break'),
+(47, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Last day to enroll online'),
+(48, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'First day of Spring Break'),
+(46, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'First day of classes'),
+(42, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Thanksgiving Break Last Day'),
+(45, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Enrollment Appointments begin'),
+(41, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Thanksgiving Break First Day'),
+(39, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Stop Day'),
+(40, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Last day of classes'),
+(38, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'First day of fall break'),
+(37, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Last Day of Fall Break'),
+(36, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'First Day of Fall Break'),
+(35, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'All of our code is due!'),
+(44, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Diplomas available'),
+(43, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'n', 'Nicks Birthday');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
